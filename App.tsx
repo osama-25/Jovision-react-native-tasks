9f9633397736bd5b36d58127258f7bf31675a5db
osama-25 <osamasami208@gmail.com>
2024-07-08 12:54:00 +0300

    Task-19

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { Text } from "react-native"
 import Task16 from "./Tasks/Task16"
 import Task17 from "./Tasks/Task17"
 import Task18 from "./Tasks/Task18"
+import Task19 from "./Tasks/Task19"
 const App = () => {
   return(
-    <Task18/>
+    <Task19/>
   ); 
 }
 export default App
