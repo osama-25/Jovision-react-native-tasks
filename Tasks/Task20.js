c11de93e6f7238f22399d5673e2517c0e95e274e
osama-25 <osamasami208@gmail.com>
2024-07-08 13:24:55 +0300

    Task-20

---
+++ Tasks/Task20.js
@@ -0,0 +1,49 @@
+import React, { Component, useState } from "react"
+import { Text, View, StyleSheet, Button } from "react-native";
+
+class App extends Component {
+    componentDidMount(){
+      console.log('class component loaded');
+    }
+    componentWillUnmount(){
+      console.log('class component unloaded');
+    }
+    render () {
+        return (
+            <Text>Hello World!</Text>
+        )
+    }
+}
+const Task20 = () => {
+    const titles = ["Show", "Hide"];
+    const [titleIndex, setTitleIndex] = useState(0);
+    const [showText, setshowText] = useState(false);
+    const handlePress = () => {
+        setshowText(!showText);
+        setTitleIndex((titleIndex + 1) % titles.length);
+    }
+    return (
+        <View style={styles.container}>
+            <Button title={titles[titleIndex]} onPress={handlePress}></Button>
+            {showText && <App/>}
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    text: {
+      marginTop: 10,
+      fontSize: 16,
+      color: '#333333',
+    },
+    name: {
+      fontSize: 24,
+      color: '#333333',
+    },
+  });
+export default Task20
