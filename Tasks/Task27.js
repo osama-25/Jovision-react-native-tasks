d2e4bc8dd8a0991496df8a17f3b4fced9b35855e
osama-25 <osamasami208@gmail.com>
2024-07-11 12:12:42 +0300

    Task-27

---
+++ Tasks/Task27.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react"
+import { Image, View, Button, StyleSheet, Alert } from "react-native";
+
+const Task27 = () => {
+    const images={
+      0: require('./Resources/0.jpeg'),
+      1: require('./Resources/1.jpeg'),
+      2: require('./Resources/2.jpeg')
+    };
+    const [imageIndex, setIndex] = useState(0);
+    const HandleAlert = () => {
+      Alert.alert(
+        'Image',
+        'Choose image',
+        [
+          {
+            text: '1',
+            onPress: () => setIndex(0)
+          },
+          {
+            text: '2',
+            onPress: () => setIndex(1)
+          },
+          {
+            text: '3',
+            onPress: () => setIndex(2)
+          }
+        ]
+      )
+    }
+    return(
+        <View style={styles.container}>
+            <Button title={'Change Image'} onPress={HandleAlert}></Button>
+            <Image
+                style={styles.image}
+                source={images[imageIndex]}
+            />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    image: {
+      marginTop: 50,
+      height: 400,
+      width: 400,
+    },
+  });
+export default Task27
