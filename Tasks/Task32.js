51beb49685ba3c9cb5db87c288f533356fe5f79a
osama-25 <osamasami208@gmail.com>
2024-07-23 12:00:25 +0300

    Task32

--- Tasks/Task32.js
+++ Tasks/Task32.js
@@ -1,34 +1,45 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import Video from "react-native-video";
 
-const onBuffer = (buffer) => {
-    console.log('Buffering...', buffer);
-};
+const { width } = Dimensions.get('window');
 
-const videoError = (error) => {
-    console.error('Video Error:', error);
-};
 const Task32 = () => {
+    const [paused, setPaused] = useState('true');
+
+    const onBuffer = (buffer) => {
+        console.log('Buffering...', buffer);
+    };
+    const videoError = (error) => {
+        console.error('Video Error:', error);
+    };
+    
     return(
         <View style={styles.container}>
+            
             <Video
                 source={require('../Resources/nature-video.mp4')}
                 style={styles.backgroundVideo}
                 onBuffer={onBuffer}
                 onError={videoError}
-                repeat={true}
+                controls={true}
+                resizeMode='contain'
+                paused={true}
+                onLoad={() => setPaused(false)}
             />
+            {paused && <Image style={styles.image} source={require('../Resources/nature-video-poster.jpg')}/>}
         </View>
     );
 }
 var styles = StyleSheet.create({
+    image: {
+        height: 180,
+        width: 400,
+    },
     backgroundVideo: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
+        position: 'absolute',
+        width: width,
+        height: (width * 9) / 16,
     },
     container: {
         flex: 1,
