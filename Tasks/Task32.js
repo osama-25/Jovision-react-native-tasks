8376dfa791fdaf04dc38a2e57772640f75ed1ff8
osama-25 <osamasami208@gmail.com>
2024-07-23 12:29:26 +0300

    Task 33

--- Tasks/Task32.js
+++ Tasks/Task32.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, Pressable, StyleSheet, Text, View } from "react-native";
 import Video from "react-native-video";
 
 const { width } = Dimensions.get('window');
 
 const Task32 = () => {
-    const [paused, setPaused] = useState('true');
+    const [paused, setPaused] = useState(false);
 
     const onBuffer = (buffer) => {
         console.log('Buffering...', buffer);
@@ -13,31 +13,39 @@ const Task32 = () => {
     const videoError = (error) => {
         console.error('Video Error:', error);
     };
-    
-    return(
+    const togglePause = () => {
+        setPaused(!paused);
+    };
+
+    return (
         <View style={styles.container}>
-            
-            <Video
-                source={require('../Resources/nature-video.mp4')}
-                style={styles.backgroundVideo}
-                onBuffer={onBuffer}
-                onError={videoError}
-                controls={true}
-                resizeMode='contain'
-                paused={true}
-                onLoad={() => setPaused(false)}
-            />
-            {paused && <Image style={styles.image} source={require('../Resources/nature-video-poster.jpg')}/>}
+            <Pressable onPress={togglePause}>
+                <Video
+                    source={require('../Resources/nature-video.mp4')}
+                    style={styles.backgroundVideo}
+                    onBuffer={onBuffer}
+                    onError={videoError}
+                    resizeMode='contain'
+                    paused={paused}
+                    onLoad={() => setPaused(false)}
+                    repeat={true}
+                />
+                {paused && <Image 
+                            style={styles.image} 
+                            source={require('../Resources/nature-video-poster.jpg')} 
+                            />}
+            </Pressable>
         </View>
     );
 }
 var styles = StyleSheet.create({
     image: {
+        position: 'absolute',
+        width: width,
         height: 180,
-        width: 400,
+        top: 10,
     },
     backgroundVideo: {
-        position: 'absolute',
         width: width,
         height: (width * 9) / 16,
     },
